eb6a6620c163e283606f1e5a18e1d963c8a9a854
BaptisteNVM <baptiste.viloin@newvoicemedia.com>
2019-11-16 16:47:32 +0000

    refactoring

--- src/App.test.js
+++ src/App.test.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import {configure, shallow} from "enzyme";
-import Adapter from 'enzyme-adapter-react-16';
-import Contact from './Contact';
-import PageTopImage from './parts/PageTopImage';
-import Presentation from './Presentation';
-import Specialite from './Specialites';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import Contact from "./Contact";
+import PageTopImage from "./parts/PageTopImage";
+import Presentation from "./Presentation";
+import Specialite from "./Specialites";
 configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
+it("renders without crashing", () => {
+  const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
 
 it("contains an image at the top of each page", () => {
-  const pages = [shallow(<Contact />), shallow(<Presentation />), shallow(<Specialite />)]
-  pages.forEach((page) => {
+  const pages = [
+    shallow(<Contact />),
+    shallow(<Presentation />),
+    shallow(<Specialite />)
+  ];
+  pages.forEach(page => {
     expect(page.find(PageTopImage)).toHaveLength(1);
-  })
-})
+  });
+});
